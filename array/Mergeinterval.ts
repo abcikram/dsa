60fe69bbaa863658f8fdd8d17a2c1995fd6900ce
abcikram <110589147+abcikram@users.noreply.github.com>
2026-01-07 01:49:04 +0530

    fixed

---
+++ array/Mergeinterval.ts
@@ -0,0 +1,38 @@
+function merge(intervals: number[][]): number[][] {
+  if (intervals.length <= 1) return intervals;
+
+  //sort of intervals
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  //initialte the array
+  let result = new Array();
+
+  // initiate the 1st index of intervals
+  let newInterval: number[] = intervals[0];
+
+  // push on result
+  result.push(newInterval);
+
+  for (let interval of intervals) {
+    // so if last element interval is less than newInterval 1st position
+    //then overlap [1,3],[1,3]  , 3>1 overlap , so update [1,3]
+    // [1,3] , [2,6] , 2<= 3 overlap , max(3,6) = 6 , so => [1,6]
+    // [2,6] , [8,10] , 8 !<= 6 so [ [ 1, 6 ], [ 8, 10 ] ]
+    // [8,10] [15,18] , 15 !<=10 so [ [ 1, 6 ], [ 8, 10 ], [ 15, 18 ] ]
+    if (interval[0] <= newInterval[1]) {
+      // console.log(interval,"<<<<", newInterval)
+      // means overlap
+      // here we do new Inteval 1 st and interval 1st index max , it update
+      // matinterval 1st index
+      newInterval[1] = Math.max(newInterval[1], interval[1]);
+    } else {
+      // Disjoint interval
+      // add the new interval to the list
+      newInterval = interval;
+      result.push(newInterval);
+
+      //    console.log("result", result)
+    }
+  }
+  return result;
+}
